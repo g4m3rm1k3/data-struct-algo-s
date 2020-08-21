2234adb539f6dd7f20456975997ae73f51a95512
Michael <g4m3rm1k3@hotmail.com>
2020-08-21 08:19:01 +0000

    finished merge sort

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
-def fib_recur(n):
-  if n == 0:
-    return 0
-  elif n == 1:
-    return 1
-    return fib_recur(n-1) + fib_recur(n-2)
 
-def long_fib(n):
-  if n == 0:
-    return 0
-  elif n == 1:
-    return 1
+def merge_sorted(arr1, arr2):
+  print("Merge function called with lists below:")
+  print(f"left: {arr1} and right: {arr2}")
+  sorted_arr = []
+  i, j = 0, 0
+  while i < len(arr1) and j < len(arr2):
+    if arr1[i] < arr2[j]:
+      sorted_arr.append(arr1[i])
+      i += 1
+    else:
+      sorted_arr.append(arr2[j])
+      j += 1
+    print(sorted_arr)
+  while i < len(arr1):
+    sorted_arr.append(arr1[i])
+    i += 1
+  while j < len(arr2):
+    sorted_arr.append(arr2[j])
+    j += 1
+  return sorted_arr
+
+
+def divide_arr(arr):
+  if len(arr) < 2:
+    print(f"Base condition reached with {arr[:]}")
+    return arr[:]
   else:
-    prev = 0
-    next = 1
-    for i in range(n-1):
-      print(f"{prev} + {next} = {prev + next}")
-      prev, next = next, prev + next
-    return next
-
-# print(long_fib(10))
-print(long_fib(1))
-# print(long_fib(2))
+    middle = len(arr)//2
+    print("Current list to work with:", arr)
+    print("Left split:", arr[:middle])
+    print("Right split", arr[middle:])
+    l1 = divide_arr(arr[:middle])
+    l2 = divide_arr(arr[middle:])
+    return(merge_sorted(l1, l2))
+    # implied return None
 
 
-def fib_runner(z):
-  print(f"The {z} number in the fibonacci sequence is {fib_recur(z)}")
+l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+# l = [8, 6, 2, 5]
+print(divide_arr(l))
 
-z = 0
-fib_runner(z)
-z = 1
-fib_runner(z)
-z = 10
-fib_runner(z)
+# XXXXXXXXXXX PROGRAM EXECUTION XXXXXXXXXX
+# l1 = [2,4,6,8,10]
+# l2 = [1,3,5,7,8,9]
