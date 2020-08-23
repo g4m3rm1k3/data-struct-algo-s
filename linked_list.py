7d22d640bb52a3839ae0a5413543af635e457b18
Michael <g4m3rm1k3@hotmail.com>
2020-08-23 10:12:21 +0000

    completed the linked list up to recursion reverse

--- linked_list.py
+++ linked_list.py
@@ -1,40 +1,91 @@
 class Node:
 
   def __init__(self, data=None):
-    pass
+    self.data = data
+    self.next = None
 
   def __str__(self):
-    pass
+    return f"{self.data}"
 
 class LinkedList:
 
   def __init__(self):
-    pass
+    self.head = None
+    self.tail = None
 
   def append_val(self, x):
     '''add x to the end of the list'''
-    pass
+    if not isinstance(x, Node):
+      x = Node(x)
+    if self.head == None: self.head = x
+    else: self.tail.next = x
+    self.tail = x
+
 
   def __str__(self):
-    pass
+    to_print = ""
+    current = self.head
+    while current is not None:
+      to_print += str(current.data) +'->'
+      current = current.next
+    if to_print:
+      return f"[{to_print[:-2]}]"
+    return "[]"
 
   def add_to_start(self, x):
     '''add x to teh left of the list making it the head'''
+    if not isinstance(x, Node):
+      x = Node(x)
+    x.next, self.head = self.head, x
+      
+
+
+
+
 
   def search_val(self, x):
     '''return indices where x was found'''
-    pass
+    current = self.head
+    count = 0
+    while current is not None:
+      if x == current.data:
+        return count
+      current = current.next
+      count += 1
+    return None
 
   def remove_val_by_index(self, x):
     '''remove and return value at index x provided as paramter'''
-    pass
+    if x == 0:
+      self.head = self.head.next
+    else:
+      current = self.head
+      next = current.next
+      count = 1
+      while next is not None:
+        if count == x:
+          current.next = next.next
+          return current.next
+        current = current.next
+        next = next.next
+        count += 1
+
+
+        
 
   def length(self):
     '''return the length of the list, rep'd by number of nodes'''
-    pass
+    count = 0
+    current = self.head
+    while current is not None:
+      current = current.next
+      count += 1
+    return count
 
   def reverse_list_recur(self, current, previous):
     '''reverse the sequence of node pointers in the linked list'''
-    pass
+    current = self.tail
+    while current is not None:
+
 
   
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 from linked_list import *
+
+my_list = LinkedList()
+
+for i in range(10):
+  my_list.append_val(i)
+
+print(my_list)
+
+my_list.add_to_start(5)
+my_list.add_to_start(15)
+my_list.add_to_start(35)
+my_list.add_to_start(52)
+print(my_list)
+print(my_list.length())
+print(my_list.search_val(5))
+my_list.remove_val_by_index(my_list.search_val(5))
+my_list.remove_val_by_index(my_list.search_val(5))
+print(my_list.search_val(15))
+print(my_list)
+print(my_list.length())
